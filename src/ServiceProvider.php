dcd2db7f730758d58443ed0243e1b6a85b3208f8
Chris Smith <chris@xedi.com>
2018-11-26 15:41:19 +0000

    [master] Added Service Provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Xedi\Behat\Laravel;
+
+use Illuminate\Support\ServiceProvider as BaseProvider;
+
+class ServiceProvider extends BaseProvider
+{
+    public function boot()
+    {
+    }
+}
